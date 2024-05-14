80f4f620862ae946aeaf825677fe4983a72e17c8
MelquiProgramador <mel38503@gmail.com>
2024-05-14 14:47:56 -0300

    quarto commit

--- comercio194/src/main/java/br/com/senai/comercio194/controller/VendaController.java
+++ comercio194/src/main/java/br/com/senai/comercio194/controller/VendaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,4 +32,10 @@ public class VendaController {
         List<Venda> vendas = vendaService.findAll();
         return ResponseEntity.ok().body(vendas);
     } 
+
+    // pega a venda pelo id
+    @GetMapping("/{id}")
+    public ResponseEntity<Venda> findById(@PathVariable Long id){
+        return ResponseEntity.ok().body(vendaService.findById(id));
+    }
 }
